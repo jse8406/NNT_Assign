bdd6061f83efca84c8918bc594332ffe3f5d6bae
jse <73538772+jse8406@users.noreply.github.com>
2025-02-20 10:57:38 +0900

    FEAT : calculate requirements

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,23 +3,49 @@ import pandas as pd
 # CSV 파일 로드 (메모리 경고 방지)
 df = pd.read_csv("Data/appsflyer.csv", low_memory=False)
 
-# "Install", "Complete Registration", "Purchase" 수 계산
-install_count = (df["Event Name"] == "install").sum()
+# "Install", "Complete Registration", "Purchase" 계산
+install_df = df[df["Event Name"] == "install"]
+registered_users = install_df["Customer User ID"].dropna().nunique()
+non_registered_users = install_df.loc[install_df["Customer User ID"].isna(), "AppsFlyer ID"].nunique()
+install_users = registered_users + non_registered_users
+
 complete_registration_count = (df["Event Name"] == "af_complete_registration").sum()
+
 purchase_count = (df["Event Name"] == "af_purchase").sum()
 
-# "Purchase한 유저 수" 계산 (중복 제거)
+# "Purchase한 유저 수" 계산
 purchase_users = df.loc[df["Event Name"] == "af_purchase", "Customer User ID"].nunique()
 
-# 만약 'Customer User ID'가 NaN인 경우 'AppsFlyer ID'를 사용하여 유저 수 계산
-if purchase_users == 0:  
-    purchase_users = df.loc[df["Event Name"] == "af_purchase", "AppsFlyer ID"].nunique()
-
-# 결과 출력
-print(f"Install Count: {install_count}")
-print(f"Complete Registration Count: {complete_registration_count}")
-print(f"Purchase Count: {purchase_count}")
-print(f"Purchase User Count: {purchase_users}")  # 구매한 유저 수
+# print(f"Install Count: {install_users}")
+# print(f"Complete Registration Count: {complete_registration_count}")
+# print(f"Purchase Count: {purchase_count}")
+# print(f"Purchase User Count: {purchase_users}")  # 구매한 유저 수
 
 
 # 광고 비용에 해당하는 컬럼 : cost, bidAmount, billed_charge, mobile_conversion_spendt_credits, localSpend
+
+
+ad_channel = ["apple_adchannel", "moloco_adchannel", "twitter_adchannel"]
+cost_sum = 0
+
+for channel in ad_channel:
+    df = pd.read_csv(f"Data/{channel}.csv", low_memory=False)    
+    for col in ["cost", "bidAmount", "billed_charge", "localSpend", "mobile_conversion_spent_credits"]:
+        if col in df.columns: 
+            cost_sum += df[col].sum()
+            # print(cost_sum)
+            
+        
+CVR = complete_registration_count / install_users
+CPA = cost_sum / purchase_count
+CVR = purchase_users / complete_registration_count
+CAC_signup = cost_sum / complete_registration_count
+CAC_install = cost_sum / install_users
+CAC_purchase = cost_sum / purchase_users
+
+print(f"CVR : {CVR:.3f}")
+print(f"CPA : {CPA:.3f}")
+print(f"CVR : {CVR:.3f}")
+print(f"CAC_signup : {CAC_signup:.3f}")
+print(f"CAC_install : {CAC_install:.3f}")
+print(f"CAC_purchase : {CAC_purchase:.3f}")
--- test.py
+++ test.py
@@ -1,21 +1,17 @@
 import pandas as pd
 
 # CSV 파일 로드 (메모리 경고 방지)
-df = pd.read_csv("Data/appsflyer.csv", low_memory=False)
 
-# 'Event Name'이 Install, Complete Registration, Purchase인 행 필터링 후 개수 계산
-install_count = (df["Event Name"] == "install").sum()
-complete_registration_count = (df["Event Name"] == "af_complete_registration").sum()
-purchase_count = (df["Event Name"] == "af_purchase").sum()
-
-# 총합 출력
-print(f"Install Count: {install_count}")
-print(f"Complete Registration Count: {complete_registration_count}")
-print(f"Purchase Count: {purchase_count}")
-
-# result : 
-# Install Count: 85121
-# Complete Registration Count: 33810
-# Purchase Count: 168695
+ad_channel = ["apple_adchannel", "moloco_adchannel", "twitter_adchannel"]
+cost_sum = 0
 
 
+for channel in ad_channel:
+    df = pd.read_csv(f"Data/{channel}.csv", low_memory=False)    
+    for col in ["cost", "bidAmount", "billed_charge", "localSpend", "mobile_conversion_spent_credits"]:
+        if col in df.columns: 
+            cost_sum += df[col].sum()
+            print(cost_sum)
+
+# 최종 광고 비용 출력
+print(f"Total Cost: {cost_sum}")
